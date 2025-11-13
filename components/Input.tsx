f8b885dd955da7c3b5042071f7b093cd3b6ebc1e
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-13 09:52:14 -0300

    feat: component input criado

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,11 +1,26 @@
-import React from "react"
+import React, { useId } from "react"
 
-type InputProps = React.ComponentProps<"input">
+type InputProps = React.ComponentProps<"input"> & {
+  label?: string
+}
 
-export default function Input({...props}: InputProps){
+export default function Input({ label, ...props }: InputProps) {
 
-  return(
-    <input className="mt-1 block w-full rounded-md border border-slate-300 px-3 py-2 shadow-sm placeholder-slate-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500"
-    {...props} />
+  const id = useId()
+
+  return (
+    <div>
+      {label &&
+        <label
+          htmlFor={id}
+          className="block text-sm font-medium text-slate-700"
+        >
+          {label}
+        </label>
+      }
+
+      <input id={id} className="mt-1 block w-full rounded-md border border-slate-300 px-3 py-2 shadow-sm placeholder-slate-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500"
+        {...props} />
+    </div>
   )
 }
