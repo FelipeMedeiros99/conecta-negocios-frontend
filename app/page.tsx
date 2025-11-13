79505d7896770dc7bd97de52f9912b2b0b9b426c
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-13 19:10:20 -0300

    feat: transferencia de página criada

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
+"use client"
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter()
+  useEffect(()=>{
+    if(localStorage.getItem("token")){
+      router.push("/home")
+      return
+    }
+    router.push("login")
+  }, [])
   return (
     <div></div>
   );
