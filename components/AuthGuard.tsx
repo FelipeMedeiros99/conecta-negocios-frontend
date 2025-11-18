bab9baa15bfb31baada0a9f4a7506a29eb92b729
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-18 15:07:44 -0300

    feat: guarda para usuários sem token criado

---
+++ components/AuthGuard.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { ReactNode, useEffect } from "react"
+import { useRouter } from "next/navigation";
+
+export default function AuthGuard({ children }: { children: ReactNode }) {
+  const router = useRouter()
+  useEffect(() => {
+    if (window !== undefined) {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        router.push("/login")
+      }
+    }
+  })
+  return (
+    <>
+      {children}
+    </>
+  )
+}
