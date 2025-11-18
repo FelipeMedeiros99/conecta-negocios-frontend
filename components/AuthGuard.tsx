30e7d00ea66b19120af655745649fa17cbf86ec0
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-18 15:09:40 -0300

    cleanup: redundancia rmeovida

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -5,13 +5,12 @@ import { useRouter } from "next/navigation";
 export default function AuthGuard({ children }: { children: ReactNode }) {
   const router = useRouter()
   useEffect(() => {
-    if (window !== undefined) {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        router.push("/login")
-      }
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login")
     }
-  })
+  }
+  )
   return (
     <>
       {children}
