8a11c3a4fd55c1d8c1f75a843df1aa20ba448410
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-18 14:08:11 -0300

    feat: layout criado para páginas restritas

---
+++ app/(privado)/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <React.Fragment>
+      <Navbar />
+
+      {children}
+    </React.Fragment>
+
+
+  );
+}
